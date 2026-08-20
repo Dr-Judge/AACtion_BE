cc726d7a37607596164098cb8158bf30002e0a13
sooyoung0927 <sooyoung040927@gmail.com>
2026-08-21 02:40:58 +0900

    Update SecurityConfig.java

--- src/main/java/com/likelion/drjudge/global/config/SecurityConfig.java
+++ src/main/java/com/likelion/drjudge/global/config/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "https://drjudge.netlify.app"
+                "https://drjudge.netlify.app",
+            "https://dr-judge.netlify.app
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of(
